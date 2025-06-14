a99050d130567159529e417eab1a7ed2b0e62608
wathi <68807000+wathi@users.noreply.github.com>
2025-06-14 18:30:05 +0100

    MLM-1 <change home page>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
   return (
     <div>
       <div className="nav">Nav Bar</div>
+      <div className="sidebar">Side bar</div>
       <div className="content">Content</div>
     </div>
   );
