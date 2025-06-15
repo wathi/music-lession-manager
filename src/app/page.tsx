5d91b74e5dc0a407fe8c7dbaefd530ed02e8da07
wathi <68807000+wathi@users.noreply.github.com>
2025-06-15 16:40:23 +0100

    MLM-1 <update homepage>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,49 @@
 export default function Page() {
   return (
-    <div className="h-screen w-screen bg-stone-300 grid place-content-center">
-      <div className="flex flex-col justify-center">
-        <div className="text-6xl mb-20">Music Lesson Manager</div>
-        <div className="flex justify-center">
-          <button className="px-8 py-4 m-4 rounded-xl bg-emerald-900 text-white hover:bg-green-800">
-            Sign up
-          </button>
-          <button className="px-8 py-4 m-4 rounded-xl border border-emerald-900 text-black hover:bg-stone-400">
-            Sign in
-          </button>
-        </div>
+    <>
+      <div className="mx-80 p-6">
+        <nav className="text-gray-800 text-md flex items-center justify-between">
+          <div className="text-xl cursor-pointer">NEIRO</div>
+          <div className="flex items-center">
+            <div className="p-4 py-2 mx-1 rounded-xl hover:bg-gray-300 cursor-pointer">
+              Feature
+            </div>
+            <div className="px-4 py-2 mx-1 rounded-xl hover:bg-gray-300 cursor-pointer">
+              Pricing
+            </div>
+            <div className="px-4 py-2 mx-1 rounded-xl hover:bg-gray-300 cursor-pointer">
+              Contact
+            </div>
+            <div className="px-4 py-2 mx-1 rounded-xl hover:bg-gray-300 cursor-pointer">
+              Log in
+            </div>
+          </div>
+        </nav>
+        <main className="flex flex-col items-center p-4 m-20">
+          <div className="text-gray-900 text-5xl font-bold p-6 mb-6">
+            Where every lesson finds its rhythm
+          </div>
+          <p className="text-gray-800 text-2xl">
+            A refined space to harmonise music learning —
+          </p>
+          <p className="text-gray-800 text-2xl mb-10">
+            schedule, organise, and grow your musical journey.
+          </p>
+          <div className="rounded-md px-8 py-3 bg-gray-800 text-white font-medium hover:bg-gray-700 cursor-pointer">
+            Join Now
+          </div>
+        </main>
       </div>
-    </div>
+      <footer className="bg-gray-700 text-white">
+        <div className="mx-80 p-6">
+          <div className="text-xl mb-2">NEIRO</div>
+          <div className="text-sm mb-2">Follow us</div>
+          <div className="border-b mb-2"></div>
+          <div className="text-xs">
+            Copyright (C) 2025. All Rights Reserved.
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
