d9ba1303e2b0c0f704b3bf45b32efaa8b949d46d
wathi <68807000+wathi@users.noreply.github.com>
2025-06-19 21:02:16 +0100

    MLM-16 <arrange file structure>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function DashboardPage() {
-  return <div></div>;
+import { redirect } from 'next/navigation';
+import { createClient } from 'utils/supabase/server';
+
+export default async function DashboardPage() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/login'); // Protect this route
+  }
+
+  return (
+    <>
+      <div>Hello, First name</div>
+      <div></div>
+    </>
+  );
 }
