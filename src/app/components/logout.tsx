4a0602a4ce96fd5a7821d91cae2989a02b9f7ed0
wathi <68807000+wathi@users.noreply.github.com>
2025-06-21 18:25:53 +0100

    MLM-16 <add edit profile function, change file structure>

---
+++ src/app/components/logout.tsx
@@ -0,0 +1,11 @@
+{
+  /* <button
+  onClick={async () => {
+    await supabase.auth.signOut();
+    window.location.href = '/login';
+  }}
+  className="px-3 py-1 rounded bg-gray-600 hover:bg-gray-500"
+>
+  Logout
+</button>; */
+}
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
 import { createClient } from 'utils/supabase/server';
+import EditProfileForm from 'app/components/edit-profile-form';
 
 export default async function ProfilePage() {
   const supabase = await createClient();
@@ -16,58 +17,15 @@ export default async function ProfilePage() {
   const { data: profile, error: profileError } = await supabase
     .from('profiles')
     .select('*')
-    .eq('id', user.id)
-    .single(); // since each user has one profile
+    .eq('user_id', user.id)
+    .single();
 
   if (profileError) throw profileError;
 
-  type Account = {
-    id: string;
-    name: string;
-    subdomain: string;
-  };
-
-  type AccountUser = {
-    accounts: Account | null;
-  };
-
-  const { data: accounts } = (await supabase
-    .from('account_users')
-    .select('accounts ( id, name, subdomain )')
-    .eq('user_id', user.id)) as { data: AccountUser[] };
-
   return (
     <div className="px-50">
-      <h1 className="mb-6 text-2xl font-bold">Profile settings</h1>
-      <div className="mb-6 border-b">
-        <div className="font-bold">First name</div>
-        <div className="py-2 mb-2">{profile.first_name}</div>
-        <div className="font-bold">Last name</div>
-        <div className="py-2 mb-2">{profile.last_name}</div>
-        <div className="font-bold">Email</div>
-        <div className="py-2 mb-2">{user.email}</div>
-      </div>
-
-      {accounts?.map(
-        ({ accounts }) =>
-          accounts && (
-            <div
-              key={accounts.id}
-              className="flex items-center justify-between"
-            >
-              <div>
-                <div className="text-xl text-gray-700">{accounts.name}</div>
-                <a
-                  className="text-sm text-gray-500"
-                  href={`${accounts.subdomain}`}
-                >
-                  {accounts.subdomain}
-                </a>
-              </div>
-              <div className="bg-gray-300 px-6 py-2 rounded-md">Setting</div>
-            </div>
-          )
-      )}
+      <h1 className="mb-6 text-2xl font-bold">Profile</h1>
+      <EditProfileForm user={user} profile={profile} />
     </div>
   );
 }
